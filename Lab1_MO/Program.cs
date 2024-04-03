75c7c7039abb84b9e42f589b5eee95fd5e4ca9fa
artemef1996@gmail.com <artemef1996@gmail.com>
2024-04-03 22:26:21 +0300

    formating

--- Lab1_MO/Program.cs
+++ Lab1_MO/Program.cs
@@ -2,34 +2,23 @@
 
 class BisectionMethod
 {
-    static double Function(double x)
+    static double F(double x) =>  2 * Math.Pow(x, 3) + 9 * Math.Pow(x, 2) - 21;
+
+    static double Bisection(double a, double b, double e)
     {
-        var v = 2 * x * x * x + 9 * x * x - 21; //2 * Math.Pow(x, 3) + 9 * Math.Pow(x, 2) - 21;
-
-        return v; // Пример: квадратичная функция
-    }
-
-    static double Bisection(double a, double b, double epsilon)
-    {
-        double fa = Function(a);
-        double fb = Function(b);
-
-        if (fa * fb >= 0)
+        var k = 0;
+       
+        while ((b - a) > e)
         {
-            throw new ArgumentException("Функция должна иметь разные знаки на концах интервала [a, b]");
-        }
+            var xc = (a + b) / 2; 
+            var halfLength = (b - a) / 2;
+            var yc = a + halfLength / 4; 
+            var zc = b - halfLength / 4; 
+            var fc = F(xc);
+            var fy = F(yc);
+            var fz = F(zc);
 
-        while ((b - a) > epsilon)
-        {
-            double xc = (a + b) / 2; // Средняя точка интервала
-            double halfLength = (b - a) / 2; // Длина половины интервала
-
-            double yc = a + halfLength / 4; // Точка y
-            double zc = b - halfLength / 4; // Точка z
-
-            double fc = Function(xc);
-            double fy = Function(yc);
-            double fz = Function(zc);
+            Output(k, a, b, xc, halfLength, yc, zc, fc, fy, fz);
 
             if (fy < fc)
             {
@@ -49,6 +38,10 @@ class BisectionMethod
                     b = zc;
                 }
             }
+
+            k++;
+
+            Output(k, a, b, xc, halfLength, yc, zc, fc,fy,fz);
         }
 
         return (a + b) / 2;
@@ -56,20 +49,27 @@ class BisectionMethod
 
     static void Main(string[] args)
     {
-        double a = -1; // Левая граница интервала
-        double b = 3; // Правая граница интервала
-        double epsilon = 0.3; // Требуемая точность
+        double a = -1;
+        double b = 3; 
+        double e = 0.3;
+        
+        double root = Math.Round(Bisection(a, b, e), 6);
 
-        try
-        {
-            double root = Bisection(a, b, epsilon);
-            Console.WriteLine($"Корень функции на интервале [{a}, {b}] с точностью {epsilon} равен {root}");
+        Console.WriteLine($"Корень функции на интервале [{a}, {b}] с точностью {e} равен {root}");
+        Console.WriteLine($"Значение функции в {root}: " + Math.Round(F(root), 6));
+    }
 
-            Console.WriteLine(Function(root));
-        }
-        catch (ArgumentException e)
-        {
-            Console.WriteLine($"Ошибка: {e.Message}");
-        }
+    static void Output(int k, double a, double b, double xc, double halfLength, double yc, double zc, double fc, double fy, double fz)
+    {
+        Console.WriteLine($"\niteration {k}:");
+        Console.WriteLine("a: " + Math.Round(a, 6) + "\n" +
+                          "b: " + Math.Round(b, 6) + "\n" +
+                         "xc: " + Math.Round(xc, 6) + "\n" +
+                          "halfLength: " + Math.Round(halfLength, 6) + "\n" +
+                          "yc: " + Math.Round(yc, 6) + "\n" +
+                          "zc: " + Math.Round(zc, 6) + "\n" +
+                          "fc: " + Math.Round(fc, 6) + "\n" +
+                          "fy: " + Math.Round(fy, 6) + "\n" +
+                          "fz: " + Math.Round(fz, 6) + "\n");
     }
 }
