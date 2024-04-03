3319c11d3e8b294ced744ec9845c64ec63804b7e
artemef1996@gmail.com <artemef1996@gmail.com>
2024-04-03 15:15:54 +0300

    task3

--- Lab1_MO/Program.cs
+++ Lab1_MO/Program.cs
@@ -1,77 +1,104 @@
-﻿namespace Lab1_MO
+﻿using System;
+
+class FibonacciMethod
 {
-    class GoldenSectionSearch
+    static double Function(double x)
     {
-        static double GoldenRatio = (3 - Math.Sqrt(5)) / 2; // Значение золотого сечения
+        var v = 2 * x * x * x + 9 * x * x - 21; //2 * Math.Pow(x, 3) + 9 * Math.Pow(x, 2) - 21;
 
-        static double Function(double x)
+        return v; // Пример: квадратичная функция
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+            return n;
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+
+    static int FindN(double a, double b, double l)
+    {
+        double length = b - a;
+        int N = 0;
+        while (Fibonacci(N) < length / l)
+            N++;
+        return N;
+    }
+
+    static double FibonacciSearch(double a, double b, double epsilon)
+    {
+        int N = FindN(a, b, epsilon);
+        int k = 0;
+
+        double y = a + (double)Fibonacci(N - 2) / Fibonacci(N) * (b - a);
+        double z = a + (double)Fibonacci(N - 1) / Fibonacci(N) * (b - a);
+
+        double fy = Function(y);
+        double fz = Function(z);
+
+        while (k != N - 3)
         {
-            // Здесь нужно реализовать вашу функцию
-
-            var v = 2 * x * x * x + 9 * x * x - 21; //2 * Math.Pow(x, 3) + 9 * Math.Pow(x, 2) - 21;
-
-            return v; // Пример: квадратичная функция
-        }
-
-        static double GoldenSectionSearchMin(double a, double b, double epsilon)
-        {
-            double y = a + GoldenRatio * (b - a);
-            double z = a + b - y;
-
-            double fy = Function(y);
-            double fz = Function(z);
-
-            while (Math.Abs(a - b) > epsilon)
+            if (fy <= fz)
             {
-                if (fy <= fz)
-                {
-                    a = a;
-                    b = z;
-                    z = y;
-                    y = a + b - y;
-                    fz = fy;
+                b = z;
+                z = y;
+                y = a + (double)Fibonacci(N - k - 3) / Fibonacci(N - k - 1) * (b - a);
+                fz = fy;
+                fy = Function(y);
+            }
+            else
+            {
+                a = y;
+                y = z;
+                z = a + (double)Fibonacci(N - k - 2) / Fibonacci(N - k - 1) * (b - a);
+                fy = fz;
+                fz = Function(z);
+            }
+            k++;
 
-                    fy = Function(y);
-                }
-                else
-                {
-                    a = y;
-                    b = b;
-                    y = z;
-                    z = a + b - z;
-                    fy = fz;
-                    fz = Function(z);
-                }
-
-                Console.WriteLine(a.ToString() + "\n" +
+            Console.WriteLine(a.ToString() + "\n" +
                                   b.ToString() + "\n" +
                                   z.ToString() + "\n" +
                                   y.ToString() + "\n" +
                                   fy.ToString() + "\n" +
                                   fz.ToString() + "\n");
-
-
-            }
-
-            return (a + b) / 2;
         }
 
-        static void Main(string[] args)
+        // Final N-th function evaluation
+        double yN = (a + b) / 2;
+        double zN = yN;
+
+        if (k == N - 3)
         {
-            double a = 0; // Начальная левая граница интервала
-            a = Convert.ToDouble(Console.ReadLine());
-            
-            double b = 1; // Начальная правая граница интервала
-            b = Convert.ToDouble(Console.ReadLine());
-            
-            double epsilon = 0.0001; // Требуемая точность
-
-            double min = GoldenSectionSearchMin(a, b, epsilon);
-
-            Console.WriteLine($"Минимум функции на интервале [{a}, {b}] с точностью {epsilon} равен {min}");
-
-            Console.WriteLine(Function(min));
+            yN = zN = (a + b) / 2;
+            zN = yN + epsilon;
         }
+
+        double fyN = Function(yN);
+        double fzN = Function(zN);
+
+        if (fyN <= fzN)
+        {
+            b = zN;
+        }
+        else
+        {
+            a = yN;
+        }
+
+        return (a + b) / 2;
     }
 
+    static void Main(string[] args)
+    {
+        double a = -1; // Левая граница интервала
+        double b = 3; // Правая граница интервала
+        double l = 0.3; // Допустимая длина конечного интервала
+        double epsilon = 0.3; // Константа различимости
+
+        double min = FibonacciSearch(a, b, l);
+
+        Console.WriteLine($"Минимум функции на интервале [{a}, {b}] с длиной интервала {l} и константой различимости {epsilon} равен {min}");
+        Console.WriteLine(Function(min));
+    }
 }
